b1200a63dd9d0da79ee45a98cfd7be666e712d23
Vitaliy Mikhaylyuk <VitaliyMikhaylyuk@gmail.com>
2020-05-18 11:30:23 -0400

    projects crud complete

--- data/Routes/projectRoutes.js
+++ data/Routes/projectRoutes.js
@@ -3,8 +3,9 @@ const db = require("../helpers/projectModel");
 
 const router = express.Router();
 
-router.get("/", (req,res) => {
-    db.get(req.query)
+
+router.get("/", (req, res) => {
+    db.get()
         .then(project => {
             res.status(200).json(project)
         })
@@ -21,7 +22,7 @@ router.get("/:id", (req, res) => {
     db.get(id)
         .then(project => {
             if (project) {
-                res.status(200).json(db)
+                res.status(200).json(project)
             } else {
                 res.status(404).json({ errorMessage: 'Project does not exist' })
             }
@@ -31,7 +32,6 @@ router.get("/:id", (req, res) => {
             res.status(500).json({ error: 'Project could not be retrieved' })
         })
 });
-
 
 router.post("/", (req, res) => {
     const body = req.body;
@@ -43,12 +43,49 @@ router.post("/", (req, res) => {
     .catch(error => {
         console.log(error)
         if (!error.name || !error.description) {
-            res.status(400).json({ errorMessage: 'Please provide a name and description for the post.'})
+            res.status(400).json({ errorMessage: 'Please provide a name and description for the project.'})
         } else {
-            res.status(500).json({ error: 'There was an error saving the post.'});
+            res.status(500).json({ error: 'There was an error saving the project.'});
         }
     })
 })
+
+router.put("/:id", (req, res) => {
+    const id = req.params.id;
+    const project = req.body;
+    if (!project.name || !project.description) {
+        res.status(400).json({ errorMessage: "Please provide the name and description for the project" });
+    } else {
+        db.update(id, project)
+        .then(updated => {
+            if (updated) {
+                res.status(200).json(updated);
+            } else {
+                res.status(404).json({ message: "The project with the specified ID does not exist" });
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ error: "The project information could not be modified"});
+        })
+    }
+});
+
+router.delete("/:id", (req, res) => {
+    const id = req.params.id;
+    db.remove(id)
+    .then(project => {
+        if (project) {
+            res.status(200).json({ message: "This project was deleted"})
+        } else {
+            res.status(404).json({ message: "The project with the specified ID does not exist" })
+        }
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({ error: "The project could not be deleted" })
+    })
+});
 
 module.exports = router;
 
